d317556ba674e682b593f37f4fdcfc783d5d85b0
Ishat <gstmigration1@gmail.com>
2026-05-26 18:16:13 +0530

    Added persistent Supabase voting system

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+console.log("SUPABASE URL:", supabaseUrl);
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
-);
+)
